47b79f0d45ec91116e8e63f03057c0ab39b03d3e
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-23 00:20:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9006,7 +9006,7 @@ const issuesWithUpdatedApiUrl = issues.map(function(url) {
   if (issues.url.includes('api.github.com')) {
     const update = issues.url.replace("api.github.com", 'api-v2.github.com')
     return Object.assign({}, issue, {
-      url:
+      url: [update]
     })
   }
 })
